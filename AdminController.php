ddaa2b5a582b4db014fd0b9994aad38b5ad857e3
Vytautas Samsonovas <vytas@ftw.lt>
2017-10-11 15:26:41 +0300

    Copy IP translation file to file/translation

--- AdminController.php
+++ AdminController.php
@@ -26,10 +26,27 @@ class AdminController extends \Ip\Controller {
                 $data['results'] = array_merge( $found, $currentTranslation );
                 $data['writable'] = Model::is_writable( $name, $data['query']['language'] );
             } elseif(!empty($data['query']['ip'])) {
-                $found = Model::get_current_translation( 'Ip', $data['query']['language'], null, 'original' );
-                $currentTranslation = Model::get_current_translation( 'Ip', $data['query']['language'], null, 'override' );
+                $ds = DIRECTORY_SEPARATOR;
+                $language = $data['query']['language'];
+
+                $originalTranslationFile = ipFile("file{$ds}translations{$ds}original{$ds}Ip-{$language}.json");
+
+                if (!file_exists($originalTranslationFile)) {
+                    $translationFile = ipFile("Ip{$ds}Internal{$ds}Translations{$ds}translations{$ds}Ip-{$language}.json");
+
+                    // Language file not found, default to english
+                    if (!file_exists($translationFile)) {
+                        $translationFile = ipFile("Ip{$ds}Internal{$ds}Translations{$ds}translations{$ds}Ip-en.json");
+                    }
+
+                    copy($translationFile, $originalTranslationFile);
+                }
+
+                $found = Model::get_current_translation( 'Ip', $language, null, 'original' );
+                $currentTranslation = Model::get_current_translation( 'Ip', $language, null, 'override' );
+
                 $data['results'] = array_merge( $found, $currentTranslation );
-                $data['writable'] = Model::is_writable( 'Ip', $data['query']['language'] );
+                $data['writable'] = Model::is_writable( 'Ip', $language );
             }
         }
         $data['plugins'] = Model::get_all_plugins();
